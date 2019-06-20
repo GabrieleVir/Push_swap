93dbdae37ad0bfdd24031edfecec4b7d1b8a79fa
Gabriele Virga <Gabriele_Virga@hotmail.com>
2019-06-20 03:09:50 +0200

    starting checker

---
+++ libft/srcs/ft_memalloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gvirga <gvirga@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/15 18:06:26 by gvirga            #+#    #+#             */
+/*   Updated: 2019/06/07 22:49:09 by gvirga           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	void	*mem;
+
+	mem = malloc(size);
+	if (mem == NULL)
+		return (NULL);
+	ft_memset(mem, 0, size);
+	return (mem);
+}
